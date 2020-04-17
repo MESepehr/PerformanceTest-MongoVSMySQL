c01b8a42e86d273e330e26d2413655141a21ff69
Mohammad Ebrahim Sepehr <ebrahimsepehr@gmail.com>
2020-04-17 11:58:34 +0430

    Create DB

---
+++ server.js
@@ -0,0 +1,10 @@
+console.log("Helo server");
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+ 
+MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
