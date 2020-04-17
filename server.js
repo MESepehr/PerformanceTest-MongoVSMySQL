cc8de2d76193b856ddf1bdb86d97571d9508425a
Mohammad Ebrahim Sepehr <ebrahimsepehr@gmail.com>
2020-04-17 12:16:52 +0430

    Insert data

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   var dbo = db.db("mydb");
-  dbo.createCollection("customers", function(err, res) {
+  /*dbo.createCollection("customers", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
+    db.close();
+  });*/
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers2").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
     db.close();
   });
 });
