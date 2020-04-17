79d5a959104602412b2c476f46955a12ed0a1a94
Mohammad Ebrahim Sepehr <ebrahimsepehr@gmail.com>
2020-04-17 12:07:07 +0430

    Create collection

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 console.log("Helo server");
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
  
 MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
-  db.close();
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
 });
