795148de524bf5bc2f7a81afcd34bd01cf232984
Mohammad Ebrahim Sepehr <ebrahimsepehr@gmail.com>
2020-04-17 16:00:41 +0430

    Inserts are seperated

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 console.log("Helo server");
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
- 
 var manyData = [{ name: "Company Inc", address: "Highway 37" ,phone:"09434832"}];
 manyData=[];
 var total = 10000 ;
@@ -27,66 +24,82 @@ function randomAdress()
     return randomName()+' '+randomName()+' '+randomName();
 }
 
-var tim = new Date().getTime();
+var tim ;
 
-MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  var dbo = db.db("mydb");
-  /*dbo.createCollection("customers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });*/
-  //var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("customers2").insertMany(manyData, function(err, res) {
-    if (err) throw err;
-    console.log(res.insertedCount+" document inserted: "+res.result.ok);
-    db.close();
 
-    console.log('* Insert tooks : '+(new Date().getTime()-tim));
+insertMongo();
+function insertMongo()
+{
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/mydb";
+  
+  
+  tim = new Date().getTime();
+  MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    var dbo = db.db("mydb");
+    /*dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });*/
+    //var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("customers2").insertMany(manyData, function(err, res) {
+      if (err) throw err;
+      console.log(res.insertedCount+" document inserted: "+res.result.ok);
+      db.close();
+
+      console.log('* Insert tooks : '+(new Date().getTime()-tim));
+
+      sqlInsert();
+    });
   });
-});
-
+};
 
 
 //////////////////////////MySQL
 
-console.log('\n\n**********************mySQL***********************\n\n')
-
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "performanceTest"
-});
-
-
-
-var insertQueryString = "INSERT INTO customers2 (name, adress,phone,school,class) VALUES ";
-var insertData=[];
-
-for(i = 0 ; i<manyData.length ; i++)
+function sqlInsert()
 {
-    insertQueryString = insertQueryString+"(?,?,?,?,?),";
-    insertData.push(manyData[i].name,manyData[i].address,manyData[i].phone,manyData[i].school,manyData[i].class);
-}
+  console.log('\n\n**********************mySQL***********************\n\n')
 
-insertQueryString = insertQueryString.substring(0,insertQueryString.length-1);
-//console.log(insertQueryString);
 
-tim = new Date().getTime();
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to mySQL!");
-  var sql = insertQueryString;
-  con.query(sql,insertData, function (err, result) {
-    if (err) throw err;
-    console.log(result.affectedRows + " record inserted");
-    con.destroy();
-    console.log("Inserting in mySQL tooks "+(new Date().getTime()-tim));
+  var mysql = require('mysql');
+  
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "performanceTest"
   });
-});
+  
+  
+  
+  var insertQueryString = "INSERT INTO customers2 (name, adress,phone,school,class) VALUES ";
+  var insertData=[];
+  
+  for(i = 0 ; i<manyData.length ; i++)
+  {
+      insertQueryString = insertQueryString+"(?,?,?,?,?),";
+      insertData.push(manyData[i].name,manyData[i].address,manyData[i].phone,manyData[i].school,manyData[i].class);
+  }
+  
+  insertQueryString = insertQueryString.substring(0,insertQueryString.length-1);
+  //console.log(insertQueryString);
+  
+  tim = new Date().getTime();
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to mySQL!");
+    var sql = insertQueryString;
+    con.query(sql,insertData, function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record inserted");
+      con.destroy();
+      console.log("Inserting in mySQL tooks "+(new Date().getTime()-tim));
+    });
+  });
+
+}
